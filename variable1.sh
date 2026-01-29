9e7d7d3bae2fac68bb6b8722bd146735c2eb6d89
devarachandrasekhar <chandrasekhardevara3@gmail.com>
2026-01-29 07:03:53 +0530

    Pushing ShellScript Files

--- variable1.sh
+++ variable1.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-echo "enter your username"
+# echo "enter your username"
 
-read -s username
+# read -s username
 
-echo "your user name is" $username
+# echo "your user name is" $username
+
+number=$1
+
+if [ $number -gt 0 ]
+
+then 
+  echo "The given number $number is greater then 0"
+else
+  echo "The given number $number is less then 0"
