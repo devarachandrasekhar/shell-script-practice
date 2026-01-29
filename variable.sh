353c52999c5a61c7d10cc486535d25bbb455e92f
devarachandrasekhar <chandrasekhardevara3@gmail.com>
2026-01-29 06:46:58 +0530

    Pushing ShellScript Files

--- variable.sh
+++ variable.sh
@@ -12,3 +12,10 @@ echo  $NAME1 "Hi" $NAME2 "How are you"
 
 echo $NAME2 "I am doing good ,How are you" $NAME1
 
+
+NAME1=$1
+NAME2=$2
+
+echo  $NAME1 "Hi" $NAME2 "How are you"
+
+echo $NAME2 "I am doing good ,How are you" $NAME1
