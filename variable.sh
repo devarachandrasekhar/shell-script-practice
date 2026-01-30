6c74867e2a42be5e727acaaa8bc5264b2c1dcfc4
devarachandrasekhar <chandrasekhardevara3@gmail.com>
2026-01-30 07:20:16 +0530

    Learning shell

--- variable.sh
+++ variable.sh
@@ -2,25 +2,5 @@
 
 NAME=ShellScript
 
-echo 'Learing shell' $NAME
+echo 'Learinggt' $NAME
 
-
-NAME1=damu
-NAME2=chandu
-
-echo  $NAME1 "Hi" $NAME2 "How are you"
-
-echo $NAME2 "I am doing good ,How are you" $NAME1
-
-
-NAME1=$1
-NAME2=$2
-
-echo  $NAME1 "Hi" $NAME2 "How are you"
-
-echo $NAME2 "I am doing good ,How are you" $NAME1
-
-
-DATE=$(date)
-
-echo 'Today date' $DATE
