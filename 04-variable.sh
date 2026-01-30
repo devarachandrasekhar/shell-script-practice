1151bfbcffb115634a2b6b7f38ffd7a407c595cb
devarachandrasekhar <chandrasekhardevara3@gmail.com>
2026-01-30 07:49:25 +0530

    Learning shell

--- 04-variable.sh
+++ 04-variable.sh
@@ -4,11 +4,9 @@ echo 'Please enter username'
 
 read -s USERNAME
 
-echo "user entered is:$USERNAME"
-
 echo 'Please enter Password'
 
 read -s PASSWORD
 
-
+echo "user entered is:$USERNAME"
 echo "password entered is:$PASSWORD"
