800a7a27d5f86da6b157e47f7946bce538a50af9
devarachandrasekhar <chandrasekhardevara3@gmail.com>
2026-01-30 07:56:44 +0530

    Learning shell

---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+PERSONS=('ramu','chandu','teja')
+
+PERSONS=('ramu','chandu','teja')
+
+echo "first person:${PERSONS[0]}"
+
+echo "second person:${PERSONS[1]}"
+
+echo "all person:${PERSONS[@]}"
